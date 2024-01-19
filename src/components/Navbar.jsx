bdb527dc10552df4d4be7c2c016c5e13964b530e
satwikrprabhu <satwikprabhu@gmail.com>
2024-01-19 15:08:24 +0530

    chore:initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 w-full z-20 text-center md:text-left border-b border-gray-400/20 bg-gray-800 bg-opacity-5 backdrop-blur-lg backdrop-filter p-3 flex justify-between items-center drop-shadow-xl px-4">
+        <h1 className="md:px-12 font-bold text-2xl md:text-4xl text-black dark:text-white flex flex-row justify-between items-center gap-2 md:gap-4">
+          <Image src="/favicon.png" alt="" height={20} width={50} />
+          <Link href='/'>DevSync</Link>
+          </h1>
+          <div className="flex flex-row gap-20 justify-center items-center">
+            <div className="hidden lg:flex flex-row gap-6">
+          <ul className="flex flex-row gap-12 text-lg font-semibold">
+            <Link href='/'>Home</Link>
+            <Link href='/'>About</Link>
+            <Link href='/'>Contact</Link>
+          </ul>
+          </div>
+          <div className="flex flex-row gap-4 items-center">
+            
+          </div> 
+          </div>
+    </nav>
+  )
+}
+
+export default Navbar
