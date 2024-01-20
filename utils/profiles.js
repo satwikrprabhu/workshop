9d7a7d9aa9e3a64d76b9b35f29063fe2430760de
leorasdsouza <leorastanak@gmail.com>
2024-01-20 12:44:42 +0530

    Added entry for leora

--- utils/profiles.js
+++ utils/profiles.js
@@ -9,5 +9,10 @@ export const data = [
     githubUserName: "satwikrprabhu",
     branch: "CSE",
   },
+  {
+    name: "Leora Dsouza",
+    githubUserName: "leorasdsouza",
+    branch: "ISE",
+  },
   
 ];
