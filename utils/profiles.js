570e375779cdb9d96b9f2925c40322e955d3cd66
Dinesh Acharya <dineshacharya2004@gmail.com>
2024-01-20 12:45:21 +0530

    Dinesh Acharya detials updated

--- utils/profiles.js
+++ utils/profiles.js
@@ -9,5 +9,10 @@ export const data = [
     githubUserName: "satwikrprabhu",
     branch: "CSE",
   },
+  {
+    name: "Dinesh Acharya",
+    githubUserName: "dineshacharya2004",
+    branch: "CSE",
+  },
   
 ];
