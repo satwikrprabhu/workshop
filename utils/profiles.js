8afc250de6cc956dcf2e79fb2b75acb606a23386
Raksha-2004 <144516995+Raksha-2004@users.noreply.github.com>
2024-01-20 12:49:15 +0530

    added profile

--- utils/profiles.js
+++ utils/profiles.js
@@ -9,5 +9,11 @@ export const data = [
     githubUserName: "satwikrprabhu",
     branch: "CSE",
   },
+  {
+    name: "Raksha Kamath B",
+    githubUserName: "Raksha-2004",
+    branch: "AIML",
+  },
+
   
 ];
